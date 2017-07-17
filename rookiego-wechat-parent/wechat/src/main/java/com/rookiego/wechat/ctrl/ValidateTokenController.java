a90d696b49656ba551c06245b004d4e681ab6d1b
zhanghui <1577107936@qq.com>
2017-07-17 22:50:04 +0800

    提交示例

---
+++ rookiego-wechat-parent/wechat/src/main/java/com/rookiego/wechat/ctrl/ValidateTokenController.java
@@ -0,0 +1,15 @@
+package com.rookiego.wechat.ctrl;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 微信服务器验证Controller
+ *
+ * @author zhh
+ * @date 2017年7月17日-下午10:47:58
+ * @jdk jdk1.7
+ */
+@Controller
+public class ValidateTokenController {
+
+}
